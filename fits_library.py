42648ae15515dcbcdc0529f890b335a108eb927d
scorot <scorot@gmail.com>
2023-08-15 21:00:00 +0200

    Make the script a command line tool using click

--- fits_library.py
+++ fits_library.py
@@ -5,40 +5,65 @@ the fit header for each fit file inside a directory.
 from astropy.io import fits
 from astropy.utils.data import get_pkg_data_filename
 import os
+import os.path
 import pandas as pd
 import numpy as np
+import click
 
-fits_array = []
-for root, dirs, files in os.walk("/home/seb/Pictures/Astronomie/",
-                                 topdown=False):
-    for name in files:
-        if os.path.join(root, name).endswith("fit"):
-            # print(os.path.join(root, name))
-            fits_file = os.path.join(root, name)
-            d = dict(fits.getheader(fits_file))
-            d.update({'FILENAME': name})
-            d.update({'DIRNAME': root})
-            fits_array.append(dict(d))
 
-df = pd.DataFrame(fits_array)
+def fits_library(dirname: str, csv: str) -> None:
+    fits_array = []
+    for root, dirs, files in os.walk(dirname,
+                                     topdown=False):
+        for name in files:
+            if os.path.join(root, name).endswith("fit"):
+                # print(os.path.join(root, name))
+                fits_file = os.path.join(root, name)
+                d = dict(fits.getheader(fits_file))
+                d.update({'FILENAME': name})
+                d.update({'DIRNAME': root})
+                fits_array.append(dict(d))
 
-# df[column_name].replace(
-#   [old_value1, old_value2, old_value3],
-#   [new_value1, new_value2, new_value3])
-df['FILTER'] = df['FILTER'].replace(
-    ['Bleu', 'Rouge', 'Vert', 'Filter 1', 'Filter 5', 'Filter 6', 'Filter 7'],
-    ['Blue', 'Red', 'Green', 'Luminance', 'Ha', 'OIII', 'SII'])
+    if len(fits_array) != 0:
+        df = pd.DataFrame(fits_array)
 
-# Traitement des noms d'objets manquants dans les entetes des fichiers fit
-# on se sert du nom trouvé dans le dossier
-df[['Col1', 'Object1']] = df['DIRNAME'].str.split('Astronomie/', expand=True)
-df[['OBJECT_', 'Col2']] = df['Object1'].str.split('/', n=1, expand=True)
+        # df[column_name].replace(
+        #   [old_value1, old_value2, old_value3],
+        #   [new_value1, new_value2, new_value3])
+        df['FILTER'] = df['FILTER'].replace(
+            ['Bleu', 'Rouge', 'Vert', 'Filter 1', 'Filter 5', 'Filter 6', 'Filter 7'],
+            ['Blue', 'Red', 'Green', 'Luminance', 'Ha', 'OIII', 'SII'])
 
-df['OBJECT'] = df['OBJECT'].str.strip()
-# df["OBJECT"].fillna('empty', inplace = True)
-df['OBJECT'] = np.where(df['OBJECT'].isnull(), df['OBJECT_'], df['OBJECT'])
+        # Sometimes the name of the targeted objet is not present in the fit header
+        # for this we find the name in the directory where the fit files reside.
+        # The code below supposes that the subdirectories of dirname contains
+        # objects names.
+        basedir = os.path.split(os.path.normpath(dirname))[-1]
+        df[['Col1', 'Object1']] = df['DIRNAME'].str.split(basedir, expand=True)
+        df[['OBJECT_', 'Col2']] = df['Object1'].str.split('/', n=1, expand=True)
 
-# Suppresion des colonnes inutiles
-df.drop(['Col1', 'Object1', 'Col2', 'OBJECT_'], axis=1, inplace=True)
+        df['OBJECT'] = df['OBJECT'].str.strip()
+        # df["OBJECT"].fillna('empty', inplace = True)
+        df['OBJECT'] = np.where(df['OBJECT'].isnull(), df['OBJECT_'], df['OBJECT'])
 
-df.to_csv('fits_library.csv', decimal=',')
+        # Delete useless columns
+        df.drop(['Col1', 'Object1', 'Col2', 'OBJECT_'], axis=1, inplace=True)
+
+        df.to_csv(csv, decimal='.')
+
+
+@click.command()
+@click.option('--dirname', default='./',
+              help="The directory where fit files are located")
+@click.option('--csv', default='fits_library.csv', help="The output csv file")
+def main(dirname, csv):
+    if not os.path.exists(dirname):
+        raise click.BadParameter(
+            f'Directory {dirname} does not exists.',
+            param_hint=["--dirname"], )
+    click.echo(f"Scanning directory {dirname}...")
+    fits_library(dirname, csv)
+
+
+if __name__ == '__main__':
+    main()
