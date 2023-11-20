0c77297681939c6610efdaf21e00781454007c68
scorot <scorot@gmail.com>
2023-11-20 22:12:46 +0100

    Change in if condition to reduce indentation level

--- fits_filter_fix.py
+++ fits_filter_fix.py
@@ -6,35 +6,36 @@ import click
 def filter_fix(dirname: str) -> None:
     for root, dirs, files in os.walk(dirname, topdown=False):
         for name in files:
-            if os.path.join(root, name).endswith("fits"):
+            if not os.path.join(root, name).endswith("fits"):
                 # print(os.path.join(root, name))
-                fits_file = os.path.join(root, name)
-                d = dict(fits.getheader(fits_file))
-                # print(d)
-                if "FILTER" not in d:
-                    if 'Rouge' in name:
-                        print("set Rouge for {}".format(name))
-                        fits.setval(fits_file, 'FILTER', value='Rouge')
-                    elif 'Vert' in name:
-                        print("set Vert for {}".format(name))
-                        fits.setval(fits_file, 'FILTER', value='Vert')
-                    elif 'Bleu' in name:
-                        print("set Bleu for {}".format(name))
-                        fits.setval(fits_file, 'FILTER', value='Bleu')
-                    elif 'Luminance2' in name:
-                        print("set Luminance2 for {}".format(name))
-                        fits.setval(fits_file, 'FILTER', value='Luminance2')
-                    elif '_Ha_' in name:
-                        print("set Ha for {}".format(name))
-                        fits.setval(fits_file, 'FILTER', value='Ha')
-                    elif '_OIII_' in name:
-                        print("set OIII for {}".format(name))
-                        fits.setval(fits_file, 'FILTER', value='OIII')
-                    elif '_SII_' in name:
-                        print("set SII for {}".format(name))
-                        fits.setval(fits_file, 'FILTER', value='SII')
-                    else:
-                        print("No changes in {}".format(name))
+                continue
+            fits_file = os.path.join(root, name)
+            d = dict(fits.getheader(fits_file))
+            # print(d)
+            if "FILTER" not in d:
+                if 'Rouge' in name:
+                    print("set Rouge for {}".format(name))
+                    fits.setval(fits_file, 'FILTER', value='Rouge')
+                elif 'Vert' in name:
+                    print("set Vert for {}".format(name))
+                    fits.setval(fits_file, 'FILTER', value='Vert')
+                elif 'Bleu' in name:
+                    print("set Bleu for {}".format(name))
+                    fits.setval(fits_file, 'FILTER', value='Bleu')
+                elif 'Luminance2' in name:
+                    print("set Luminance2 for {}".format(name))
+                    fits.setval(fits_file, 'FILTER', value='Luminance2')
+                elif '_Ha_' in name:
+                    print("set Ha for {}".format(name))
+                    fits.setval(fits_file, 'FILTER', value='Ha')
+                elif '_OIII_' in name:
+                    print("set OIII for {}".format(name))
+                    fits.setval(fits_file, 'FILTER', value='OIII')
+                elif '_SII_' in name:
+                    print("set SII for {}".format(name))
+                    fits.setval(fits_file, 'FILTER', value='SII')
+                else:
+                    print("No changes in {}".format(name))
 
 
 @click.command()
